d0602aebc33f683a20e2655cdae89fc133ce28d6
Wfect <85443236+Wfect@users.noreply.github.com>
2023-02-23 20:13:08 +0300

    Add files via upload

---
+++ ifelse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time<=11)
+            Console.WriteLine("İyi sabahlar");
+            else if(time<=18)
+            Console.WriteLine("İyi günler");
+            else
+            Console.WriteLine("İyi akşamlar");
+            // time <= 18 ise iyi günler değilse iyi akşamlar
+            string sonuc = time<=18 ? "İyi günler" : "İyi akşamlar";
+            Console.WriteLine(sonuc);
+        }
+    }
+}
+
