0301191168b13e666e94690c7a2e1a7145476933
Wfect <85443236+Wfect@users.noreply.github.com>
2023-02-28 15:42:00 +0300

    Add files via upload

---
+++ switchcase/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace switchcase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month; // Ayın integer karşılığını veriyor.
+
+            switch (month)
+            {
+                case 1:
+                Console.WriteLine("Ocak ayındasınız");
+                break;
+
+                case 2:
+                Console.WriteLine("Subat ayındasınız");
+                break;
+
+                default:
+                Console.WriteLine("Yanlış veri girdiniz");
+                break;
+            }
+            switch (month)// bu aylarda iseniz kış ayındasınız
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis ayındasınız");
+                    break;
+                default:
+                break;
+            }
+        }
+    }
+}
