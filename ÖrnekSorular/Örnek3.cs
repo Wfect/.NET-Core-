4e675f1b1b9c66c7814987a50550188fb9ff9ae6
Wfect <85443236+Wfect@users.noreply.github.com>
2023-03-29 18:17:46 +0300

    Add files via upload

---
+++ ÖrnekSorular/Örnek3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace örnek3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Pozitif tam sayi giriniz ");
+            int n = int.Parse(Console.ReadLine());
+
+            string [] kelimeler = new string[n];
+            for(int i = 0 ; i<n; i++){
+                Console.Write($"{i+1}. kelimeyi giriniz");
+                kelimeler [i] = (Console.ReadLine());
+            }
+            
+            Console.WriteLine("Kelimeler ");
+            for(int i = n - 1; i>=0; i--){
+                Console.WriteLine(kelimeler[i]);
+            }
+
+        }
+    }
+}
---
+++ ÖrnekSorular/Örnek4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace örnek4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Cümle giriniz : ");
+            string cümle = Console.ReadLine();
+            //Split : Cümledeki boşluk karakterlerinin dışında kelime sayısını hesaplar.
+            int kelimeSayisi = cümle.Split(' ').Length; 
+            //Replace : Boşluk karakterleri dışındaki harf sayısını hesaplar.
+            // (" "): boşlukları gösterir.
+            // ("") : boş karakteri gösterir.
+            int harfSayisi = cümle.Replace(" ", "").Length;
+
+            Console.WriteLine($"Girilen cümlede {kelimeSayisi} kelime ve {harfSayisi} harf vardır");
+        }
+    }
+}
+
+
+
